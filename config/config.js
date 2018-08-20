bff43c3fcbc7e5ea4307988be74dbcd25930b5fd
PRANAVBHALE <bhalepranav25@gmail.com>
2018-08-20 11:55:08 +0530

    Needed config file

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID':'350637015478288',
+        'clientSecret':'332645fe11d28d3cf1077a613ea898a5',
+        'callbackURL':'http://localhost:3000/auth/facebook/callback'
+    }
+}
